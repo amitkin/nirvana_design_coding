55f050d774fc673eda7bd2acb9905b4b7ca0feb4
Amit Kumar <amkumar@vmware.com>
2018-06-07 10:20:03 +0530

    Adding custom thread pool implementation

---
+++ random-practice/src/main/java/com/mylearning/parallelcomputing/threadpool/pool/BlockingQueue.java
@@ -0,0 +1,34 @@
+package com.mylearning.parallelcomputing.threadpool.pool;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BlockingQueue <Type> {
+    private Queue<Type> queue = new LinkedList<>();
+    private int EMPTY = 0;
+    private int MAX_TASK_IN_QUEUE = -1;
+
+    public BlockingQueue(int size){
+        this.MAX_TASK_IN_QUEUE = size;
+    }
+
+    public synchronized void enqueue(Type task) throws InterruptedException  {
+        while(this.queue.size() == this.MAX_TASK_IN_QUEUE) {
+            wait(); //auses the current thread to wait until another thread invokes the notify method
+        }
+        if(this.queue.size() == EMPTY) {
+            notifyAll(); //wakes up all the threads that are waiting
+        }
+        this.queue.offer(task);
+    }
+
+    public synchronized Type dequeue() throws InterruptedException{
+        while(this.queue.size() == EMPTY){
+            wait();
+        }
+        if(this.queue.size() == this.MAX_TASK_IN_QUEUE){
+            notifyAll();
+        }
+        return this.queue.poll();
+    }
+}
---
+++ random-practice/src/main/java/com/mylearning/parallelcomputing/threadpool/pool/ThreadPool.java
@@ -0,0 +1,21 @@
+package com.mylearning.parallelcomputing.threadpool.pool;
+
+public class ThreadPool {
+	
+    BlockingQueue <Runnable> queue;
+    public ThreadPool(int queueSize, int nThread) {
+        queue = new BlockingQueue<>(queueSize);
+        String threadName;
+        TaskExecutor task;
+        for (int count = 0; count < nThread; count++) {
+        	threadName = "Thread-"+count;
+        	task = new TaskExecutor(queue);
+            Thread thread = new Thread(task, threadName);
+            thread.start();
+        }
+    }
+
+    public void submitTask(Runnable task) throws InterruptedException {
+        queue.enqueue(task);
+    }
+}
