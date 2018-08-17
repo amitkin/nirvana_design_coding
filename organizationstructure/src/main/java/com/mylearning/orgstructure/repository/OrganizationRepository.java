f3581b68f087900258f9d100e2e5526bdc0d6f03
Amit Kumar <amkumar@vmware.com>
2018-08-18 00:17:20 +0530

    Added functionality to get org hierachy

--- organizationstructure/src/main/java/com/mylearning/orgstructure/repository/OrganizationRepository.java
+++ organizationstructure/src/main/java/com/mylearning/orgstructure/repository/OrganizationRepository.java
@@ -6,11 +6,14 @@ import com.mylearning.orgstructure.model.Employee;
 import com.mylearning.orgstructure.model.EmployeeMapper;
 import com.mylearning.orgstructure.model.EmployeeWithSubOrdinate;
 import com.mylearning.orgstructure.model.EmployeeWithSubOrdinateMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class OrganizationRepository {
 
@@ -19,20 +22,24 @@ public class OrganizationRepository {
     private JdbcTemplate jdbcTemplate;
 
     private final String SQL_FIND_EMPLOYEE = "select * from employee where id = ?";
-    private final String SQL_UPDATE_EMPLOYEE = "update employee set managerId = ? where id = ?";
+    private final String SQL_UPDATE_EMPLOYEE_MANAGER = "update employee set managerId = ? where id = ?";
     private final String SQL_INSERT_EMPLOYEE = "insert into employee(id, cityName, salary, firstName, secondName, managerId) values(?,?,?,?,?,?)";
     private final String SQL_EMPLOYEE_WITH_SUBORDINATES = "select * from (select b.*, count(e.id) as numSubOrdinates from "
             + "employee e\n"
             + "join employee b on b.id=e.managerid group by b.id, b.firstname) as t order by t.numSubOrdinates desc";
     private final String SQL_SALARY_ALL_SUBORDINATES = "select sum(salary) as totalsalary from employee where managerid = ?";
 
-
     public Employee getEmployeeById(Long id) {
-        return (Employee) jdbcTemplate.queryForObject(SQL_FIND_EMPLOYEE, new Object[]{id} , new EmployeeMapper());
+        try{
+            return (Employee) jdbcTemplate.queryForObject(SQL_FIND_EMPLOYEE, new Object[]{id} , new EmployeeMapper());
+        } catch(DataAccessException e){
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     public boolean updateEmployeeManager(Employee employee) {
-        return jdbcTemplate.update(SQL_UPDATE_EMPLOYEE, employee.getManagerId(), employee.getId()) > 0;
+        return jdbcTemplate.update(SQL_UPDATE_EMPLOYEE_MANAGER, employee.getManagerId(), employee.getId()) > 0;
     }
 
     public boolean createEmployee(Employee employee) {
