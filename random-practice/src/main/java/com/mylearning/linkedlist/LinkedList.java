1f90bee27731708222ff7bcafbc9bad68a45e045
Amit Kumar <amkumar@vmware.com>
2019-04-07 18:47:14 +0530

    Pushing latest changes

--- random-practice/src/main/java/com/mylearning/linkedlist/LinkedList.java
+++ random-practice/src/main/java/com/mylearning/linkedlist/LinkedList.java
@@ -212,31 +212,54 @@ public class LinkedList {
         }
     }
 
+    /**
+     * Reverse a link list between begin and end exclusively
+     * an example:
+     * a linked list:
+     * 0->1->2->3->4->5->6
+     * |           |
+     * begin       end
+     * after call begin = reverse(begin, end)
+     *
+     * 0->3->2->1->4->5->6
+     *          |  |
+     *      begin end
+     * @return the reversed list's 'begin' node, which is the precedence of node end
+     */
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (k <= 1 || head == null || head.next == null)
+        if (head==null || head.next ==null || k==1)
             return head;
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = head;
-        ListNode prev, start, then, tail;
-        tail = prev = dummyHead;
-        start = prev.next;
-        while (true) {
-            // check if there's k nodes left-out
-            for (int i = 0; i < k; i++) {
-                tail = tail.next;
-                if (tail == null)
-                    return dummyHead.next;
+        ListNode dummyhead = new ListNode(-1);
+        dummyhead.next = head;
+        ListNode begin = dummyhead;
+        int i=0;
+        while (head != null){
+            i++;
+            if (i%k == 0){
+                begin = reverse(begin, head.next);
+                head = begin.next;
+            } else {
+                head = head.next;
             }
-            // reverse k nodes
-            for (int i = 0; i < k - 1; i++) {
-                then = start.next;
-                start.next = then.next;
-                then.next = prev.next;
-                prev.next = then;
-            }
-            tail = prev = start;
-            start = prev.next;
         }
+        return dummyhead.next;
+
+    }
+
+    public ListNode reverse(ListNode begin, ListNode end){
+        ListNode curr = begin.next;
+        ListNode next, first;
+        ListNode prev = begin;
+        first = curr;
+        while (curr!=end){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        begin.next = prev;
+        first.next = curr;
+        return first;
     }
 
     public static void main(String[] args) {
