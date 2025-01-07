d2b66da8957eafe3f79e3646805a26d8fef0dccb
Amit Kumar <amit20587@gmail.com>
2025-01-07 09:41:09 +0530

    Update CycleInUnDirectedGraphDFS.java

--- random-practice/src/main/java/com/mylearning/graph/CycleInUnDirectedGraphDFS.java
+++ random-practice/src/main/java/com/mylearning/graph/CycleInUnDirectedGraphDFS.java
@@ -3,6 +3,13 @@ package com.mylearning.graph;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+If we encounter a visited vertex again, then we say, there is a cycle. But there is a catch in this algorithm,
+we need to make sure that we do not consider every edge as a cycle because in an undirected graph, an edge
+from 1 to 2 also means an edge from 2 to 1. To handle this, we keep track of the parent node (the node from which
+we came to the current node) in the DFS traversal and ignore the parent node from the visited condition.
+
+*/
 public class CycleInUnDirectedGraphDFS {
 
     Graph graph;
@@ -14,12 +21,10 @@ public class CycleInUnDirectedGraphDFS {
     public boolean hasCycleDFS(){
         Set<Integer> visited = new HashSet<>();
         for(int vertex=0; vertex<graph.getNoOfVertices() -1; vertex++){
-            if(visited.contains(vertex)){
-                continue;
-            }
-            boolean flag = hasCycleDFSUtil(vertex, visited, -1);
-            if(flag){
-                return true;
+            if(!visited.contains(vertex)){
+                if(hasCycleDFSUtil(vertex, visited, -1)) {
+                    return true;
+                }
             }
         }
         return false;
@@ -28,9 +33,11 @@ public class CycleInUnDirectedGraphDFS {
     public boolean hasCycleDFSUtil(int vertex, Set<Integer> visited, int parent){
         visited.add(vertex);
         for(int adj : graph.getNeighbours(vertex)){
+            //the node from which we came to the current node, ignore the parent node from the visited condition
             if(adj == parent){
                 continue;
             }
+            //we encounter a visited vertex again so cycle is there
             if(visited.contains(adj)){
                 return true;
             }
